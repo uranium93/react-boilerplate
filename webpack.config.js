8ab8f3ee05e79a09b951e143a0d93a83fe2b4f95
usranium93 <19hanafi93@gmail.com>
2021-01-28 01:15:36 +0400

    chore: add style loader

--- webpack.config.js
+++ webpack.config.js
@@ -45,7 +45,9 @@ const config = {
             loader: 'css-loader',
             options: {
               importLoaders: 1,
-              modules: true,
+              modules: {
+                localIdentName: '[name]__[local]___[hash:base64:5]',
+              },
             },
           },
         ],
