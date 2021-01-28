03a6161756dd9cfcc62acb2b2ec11f6f57b1f537
usranium93 <19hanafi93@gmail.com>
2021-01-28 12:53:58 +0400

    chore: add basic setup

--- src/index.tsx
+++ src/index.tsx
@@ -1,10 +1,11 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import { BrowserRouter as Router } from 'react-router-dom';
+import App from './app';
 
 ReactDOM.render(
   <Router>
-    <div>Hello Worsssld</div>
+    <App />
   </Router>,
   document.getElementById('app'),
 );
